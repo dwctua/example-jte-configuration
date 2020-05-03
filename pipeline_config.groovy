1489a8669f07a0882e8f48d3112771127cf64502
dwctua <46290157+dwctua@users.noreply.github.com>
2020-05-03 08:32:14 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -9,3 +9,17 @@ libraries{
   validation
   sonarqube
 }
+
+stages {
+  pre-build {
+     validate()
+    generate()
+  }
+  build {
+    unit_test()
+    build()
+  }
+  post-build{
+    static_code_analysis()
+  }
+}
